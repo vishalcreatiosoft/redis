e93170f5f9a4a9813021648cd7dc0054778b065f
vishalcreatiosoft <vishal.srivastava@creatiosoft.com>
2021-11-19 19:06:39 +0530

    save

--- social-site/client/public/js/client.js
+++ social-site/client/public/js/client.js
@@ -6,3 +6,13 @@
 
 //socket.emit('message','hello server');
 
+//console.log('home page');
+const data = document.querySelector('h1');
+const dataArray = JSON.parse(data.textContent);
+
+console.log(dataArray);
+data.remove()
+
+const newheading = document.createElement('h2');
+newheading.textContent = `Hii ! ${dataArray[0].firstName}`;
+document.querySelector('#welcome-msg').appendChild(newheading);
